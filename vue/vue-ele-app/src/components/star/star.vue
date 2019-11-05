224d35205fcdc7fb7c486f9542da1c59632c329c
karen <993698262@qq.com>
2019-11-05 17:02:47 +0800

    进度条面向对象编程  函数式编程

--- vue/vue-ele-app/src/components/star/star.vue
+++ vue/vue-ele-app/src/components/star/star.vue
@@ -22,16 +22,16 @@ export default {
     let star = []
     if (this.seller.foodScore > num) {
       for (let i = 0; i < 4 - num; i++) {
-        star.push('<img src="./star2.png" />')
+        star.push('<img src="../../../static/star2.png" />')
       }
-      star.push('<img src="./star-h.png" />')
+      star.push('<img src="../../../static/star-h.png" />')
     } else if (this.seller.foodScore === num) {
       for (let i = 0; i < 5 - num; i++) {
-        star.push('<img src="./star2.png" />')
+        star.push('<img src="../../../static/star2.png" />')
       }
     }
     for (let i = 0; i < num; i++) {
-      star.push('<img src="./star.png" />')
+      star.push('<img src="../../../static/star.png" />')
     }
     console.log(star)
     this.star = star.reverse().join('')
@@ -45,6 +45,8 @@ export default {
     text-align center
     margin-top 20px
     img
-      width 20px
-      height 20px
+      display inline-block
+      width 10px
+      height 10px
+      margin-left 5px
 </style>
