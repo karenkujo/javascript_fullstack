99d5483ea72d9509d20bb2b10df017f6029b1b8b
karen <993698262@qq.com>
2019-11-19 10:35:21 +0800

    修改了一个小问题

--- leetcode/candies/a.py
+++ leetcode/candies/a.py
@@ -2,8 +2,8 @@ from typing import List
 
 class Solution:
   def distributeCandies(self, candies: List[int]) -> int:
-    return min(len(ser(candies)), len(candies) >> 1)
+    return min(len(set(candies)), len(candies) >> 1)
 
 
 x = Solution()
-print(x.distributeCandies([1,1,2,2,3,3))
+print(x.distributeCandies([1,1,2,2,3,3]))
