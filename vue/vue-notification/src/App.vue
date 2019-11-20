2088c176234aa03a8e2d281907e0aa4b1c7d0435
karen <993698262@qq.com>
2019-11-20 17:06:20 +0800

    vue实现全局弹窗组件 单例模式

---
+++ vue/vue-notification/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <HelloWorld/> -->
+    <Header />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import Header from './components/header'
+import Footer from './components/footer'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Header,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue-notification/src/components/footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="footer">
+    footer
+  </div>
+</template>
+
+<script>
+import { open, close } from './notificat'
+export default {
+  mounted () {
+    setTimeout(() => {
+      open({
+        title: 'title1',
+        content: 'content1'
+      })
+      open({
+        title: 'title2',
+        content: 'content2'
+      })
+    }, 3000)
+    setTimeout(() => {
+      close()
+    }, 5000)
+  }
+}
+</script>
+
+<style scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+}
+</style>
