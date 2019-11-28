f8a502e428807e46144e4c4ba6fc34c73e0c58d1
karen <993698262@qq.com>
2019-11-28 21:39:53 +0800

    vue watch源码实现  二叉树遍历

--- vue/vue-notification/README.md
+++ vue/vue-notification/README.md
@@ -1,3 +1,8 @@
 ## 弹窗类
 全局的
 单例的
+
+-D --seve-dev  开发环境  /src
+-S --save      生产环境  /dist
+
+v-loading 自定义指令
--- vue/vue-notification/src/App.vue
+++ vue/vue-notification/src/App.vue
@@ -4,27 +4,50 @@
     <!-- <HelloWorld/> -->
     <Header />
     <Footer />
+    <el-table v-loading="loading" :data="tableData" style="width: 100%">
+      <el-table-column prop="date" label="日期" width="180"></el-table-column>
+      <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-import Header from './components/header'
-import Footer from './components/footer'
+import HelloWorld from "./components/HelloWorld";
+import Header from "./components/header";
+import Footer from "./components/footer";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
     Header,
     Footer
-  }
-}
+  },
+  data() {
+      return {
+        tableData: [{
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        loading: true
+      };
+    }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
