201f3f19289bce60ec625e1b3bddc8bec2a16c5f
karen <993698262@qq.com>
2019-11-29 22:12:21 +0800

    连接mysql查询数据

---
+++ node/noteBook/note_book/controllers/defaultConfig.js
@@ -0,0 +1,11 @@
+const config = {
+  database: {
+    DATABASE: 'test',
+    USERNAME: 'root',
+    PASSWORD: '993698262',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
