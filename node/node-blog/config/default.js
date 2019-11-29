971ce575e8ab187accd122bcf81f1af2275db140
karen <993698262@qq.com>
2019-11-29 17:23:05 +0800

    node koa学习

---
+++ node/node-blog/config/default.js
@@ -0,0 +1,14 @@
+const config = {
+  // 启动端口
+  port: 3000,
+  // 数据库配置
+  database:{
+    DATABASE: 'nodesql',
+    USERNAME: 'karen',
+    PASSWORD: '993698262',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
