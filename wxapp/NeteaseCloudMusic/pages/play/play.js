590998cb428333f28002c2bded48a214d893265e
karen <993698262@qq.com>
2019-12-02 18:38:22 +0800

    进度条(未完成)

--- wxapp/NeteaseCloudMusic/pages/play/play.js
+++ wxapp/NeteaseCloudMusic/pages/play/play.js
@@ -9,30 +9,12 @@ Page({
     id: '',
     musicUrl: '',
     name: '',
-    picUrl: ''
-  },
-  // 获取歌曲详情
-  getMusicDetail (id) {
-    let that = this
-    wx.request({
-      url: baseUrl + '/song/detail',
-      data: {
-        ids: id
-      },
-      success (res) {
-        if (res.data.code === 200) {
-          console.log(res.data)
-          wx.setNavigationBarTitle({   // 设置页面头部标题
-            title: res.data.songs[0].name  
-          })
-          that.setData({
-            name: res.data.songs[0].name,
-            picUrl: res.data.songs[0].al.picUrl
-          })
-          console.log(that.data.picUrl)
-        }
-      }
-    })
+    picUrl: '',
+    singer: '',
+    backgroundAudioManager: {},
+    duration: '00:00',
+    currentTime: '00:00',
+    state: true
   },
   // 请求音乐url
   getMusicUrl(id) {
@@ -49,17 +31,75 @@ Page({
             musicUrl: res.data.data[0].url
           })
         }
-        that.getBackgroundAudioManager()
+        that.getMusicDetail(id)
       }
+    })
+  },
+  // 获取歌曲详情
+  getMusicDetail(id) {
+    let that = this
+    wx.request({
+      url: baseUrl + '/song/detail',
+      data: {
+        ids: id
+      },
+      success(res) {
+        if (res.data.code === 200) {
+          console.log(res.data)
+          wx.setNavigationBarTitle({   // 设置页面头部标题
+            title: res.data.songs[0].name
+          })
+          that.setData({
+            name: res.data.songs[0].name,
+            picUrl: res.data.songs[0].al.picUrl,
+            singer: res.data.songs[0].ar[0].name
+          })
+          // console.log(that.data.name)
+          that.setProgress()
+        }
+      }
+    })
+  },
+  // 进度条
+  setProgress() {
+    let that = this
+    that.getBackgroundAudioManager()  // 调用创建播放器方法
+    .then((backgroundAudioManager) => {
+      that.setData({
+        backgroundAudioManager: backgroundAudioManager
+      })
+      backgroundAudioManager.onTimeUpdate(() => {
+        that.setData({
+          currentTime: Math.round(backgroundAudioManager.currentTime),
+          duration: Math.round(backgroundAudioManager.duration)
+        })
+      })
     })
   },
   // 创建音乐播放器
   getBackgroundAudioManager() {
-    const backgroundAudioManager = wx.getBackgroundAudioManager()
-    backgroundAudioManager.title = '此时此刻'
-    backgroundAudioManager.epname = '此时此刻'
-    backgroundAudioManager.singer = '许巍'
-    backgroundAudioManager.src = this.data.musicUrl
+    let that = this
+    return new Promise((resolve, reject) => {
+      const backgroundAudioManager = wx.getBackgroundAudioManager()
+      backgroundAudioManager.src = that.data.musicUrl
+      backgroundAudioManager.title = that.data.name
+      backgroundAudioManager.singer = that.data.singer
+      resolve(backgroundAudioManager)
+      reject('创建失败')
+    })
+  },
+  // 播放暂停
+  swichState() {
+    let that = this
+    that.setData({
+      state: !that.data.state
+    })
+    let backgroundAudioManager = that.data.backgroundAudioManager
+    if (that.data.state) {
+      backgroundAudioManager.play()
+    } else (
+      backgroundAudioManager.pause()
+    )
   },
   /**
    * 生命周期函数--监听页面加载
@@ -69,14 +109,13 @@ Page({
     //   id: options.id
     // })
     this.getMusicUrl(29122124)
-    this.getMusicDetail(29122124)
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
