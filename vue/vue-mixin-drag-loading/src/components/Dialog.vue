912b19af56704f4e6fa0b8be8e050b3eeda4c5bf
karen <993698262@qq.com>
2019-12-03 17:05:02 +0800

    vue-mixin  array扁平化处理

---
+++ vue/vue-mixin-drag-loading/src/components/Dialog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <span @mousedown="start" style="position:absolute; left:0; top:0">这是一个拖拽的文本</span>
+    <el-button @click="open">open</el-button>
+    <el-dialog title="tips" :visible="visible">
+      <span @click="close">这是一段信息</span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import mixin from './visible'
+import dragAbleMix from './drag'
+export default {
+  mixins: [mixin, dragAbleMix]
+}
+</script>
+
+<style>
+
+</style>
