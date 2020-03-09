afb1bee186b53bb0f06b0d30df4efa7758d9f88b
karen <993698262@qq.com>
2020-03-09 20:59:21 +0800

    redux学习

--- react-v16/react-redux/src/App.js
+++ react-v16/react-redux/src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import { Input, Button, List } from 'antd'
 import 'antd/dist/antd.css';
 import store from './store/index'
+import {CHANGE_INPUT_VALUE,ADD_TODO_ITEM,DELETE_TODO_ITEM} from './store/actionTypes'
 
 
 // store 的创建
@@ -11,20 +12,58 @@ class ToduList extends Component {
   constructor(props) {
     super(props)
     this.state = store.getState()
+    store.subscribe(this.handleStoreChange) // 只要store里面的数据发生变化，就会执行里面的方法
   }
+
+  handleInputChange = (e) => {
+    const action = {
+      type: CHANGE_INPUT_VALUE,
+      value: e.target.value
+    }
+    store.dispatch(action)
+    // this.setState(store.getState()) 
+  }
+
+  handleStoreChange = () => {
+    console.log('store changed')
+    this.setState(store.getState())
+  }
+
+  handleBtnClick = () => {
+    const action = {
+      type: ADD_TODO_ITEM
+    }
+    store.dispatch(action)
+  }
+
+  handleItemDelete (index) {
+    console.log(index)
+    const action = {
+      type: DELETE_TODO_ITEM,
+      index
+    }
+    store.dispatch(action)
+  }
+
   render() {
+    console.log(this.state.list)
     return (
       <div style={{ padding: 10 }}>
         <div>
-          <Input value={this.state.inputValue} placeholder="todo info" style={{ width: 300, marginRight: 10 }} />
-          <Button type="primary">提交</Button>
+          <Input 
+            value={this.state.inputValue} 
+            placeholder="todo info" 
+            style={{ width: 300, marginRight: 10 }} 
+            onChange={this.handleInputChange}
+            />
+          <Button onClick={this.handleBtnClick} type="primary">提交</Button>
         </div>
         <List
           style={{marginTop: 10, width: 300}}
           bordered
           dataSource={this.state.list}
-          renderItem={item => (
-            <List.Item>
+          renderItem={(item, index) => (
+            <List.Item onClick={this.handleItemDelete.bind(this, index)}>
               {item}
             </List.Item>
           )}
--- react-v16/react-redux/src/store/reducer.js
+++ react-v16/react-redux/src/store/reducer.js
@@ -1,13 +1,28 @@
+import {CHANGE_INPUT_VALUE,ADD_TODO_ITEM,DELETE_TODO_ITEM} from './actionTypes'
 const defaultState = {
-  inputValue: '123',
-  list: [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ]
+  inputValue: '',
+  list: []
 }
+// state 指的是store里面上一次存储的数据
+// action  用户传过来的那句话
 export default (state = defaultState, action) => {
+  if (action.type === CHANGE_INPUT_VALUE) {
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.inputValue = action.value
+    return newState // reducer 有一个限制，可以接收state,但是绝对不能修改state
+  }
+  if (action.type === ADD_TODO_ITEM) {
+    console.log(111)
+    const newState = JSON.parse(JSON.stringify(state))
+    newState.list.push(newState.inputValue)
+    newState.inputValue = ''
+    return newState
+  }
+  if (action.type === DELETE_TODO_ITEM) {
+    const newState = JSON.parse(JSON.stringify(state))
+    console.log(action.index)
+    newState.list.splice(action.index, 1)
+    return newState
+  }
   return state
 }
