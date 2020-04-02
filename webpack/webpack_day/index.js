7e4fd578a31dc4f600d92e12914ba6c9d1ea351f
karen <993698262@qq.com>
2020-04-02 23:44:38 +0800

    webpack学习

--- webpack/webpack_day/index.js
+++ webpack/webpack_day/index.js
@@ -1,6 +1,16 @@
 import a from './a'
 import b from './b'
 import pic from './img/timg.jpg'
+import './index.css'
+
+var img = new Image()
+img.src = pic
+img.className = "pic"
+var root = document.getElementById('root')
+root.appendChild(img)
+
+console.log(pic)
+
 
 // 既然webpack默认只认识js模块，非js模块该怎么办？
 // loader
@@ -11,4 +21,4 @@ function lm () {
   console.log('welcome to lm')
 }
 
-lm() 
+lm()
--- webpack/webpack_day/webpack.config.js
+++ webpack/webpack_day/webpack.config.js
@@ -1,11 +1,44 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: "development",
   entry: "./index.js", // 入口文件
   output: { // 出口
     // 公共路径设置
     // publicPath: "",
     path: path.resolve(__dirname, "./build"),
     filename: "index.js"
-  }
+  },
+  module: {
+    // 遇到不认识的模块，就在这里找loader
+    rules: [
+      {
+        test: /\.(jpg|jpeg|png|gif)$/,
+        use: {
+          // url-loader 可以先定模块的体积，根据体积判断是否需要转换base64，减少http请求
+          loader: "url-loader", // 就是把模块放在了另外一个目录里面。并且把位置给我们返回
+          options: {
+            // name 是打包前模块的名称，ext是打包前模块的后缀名
+            name: "[name]_[hash].[ext]",
+            outputPath: "images/",
+            limit: 1024 * 300
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"]
+      },
+      {
+        test: /\.scss$/, // loader是有执行顺序的，从后往前
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  plugins: [ new HtmlWebpackPlugin({
+    template: "./index.html",
+    title: "标题-自己取的",
+    filename: "app.html"
+  })]
 }
