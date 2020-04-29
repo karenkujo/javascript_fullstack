5833f7ef9200bc58a92967b8d6ddb0417d2ed457
karen <993698262@qq.com>
2020-04-29 11:08:42 +0800

    准备工作

---
+++ react-v16/diary/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Button } from 'antd-mobile'
+
+function App() {
+  return (
+    <div className="App">
+      <Button type="primary">111</Button>
+    </div>
+  );
+}
+
+export default App;
